0a0db89e56a3ccf1fc0a8e2f4484601255bda701
chris <chri402s@stud.kea.dk>
2022-11-24 10:23:12 +0100

    added pagefolders / navigostart (collab Kenn)

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 import "https://unpkg.com/navigo"  //Will create the global Navigo object used below
+
+import {
+    setActiveLink, adjustForMissingHash, renderTemplate, loadHtml
+} from "./utils.js"
+
+window.addEventListener("load", async () => {
+
+    const templatehome = await loadHtml("./projectPages/home.html")
+    const templateAbout = await loadHtml("./projectPages/about/about.html")
+    const templateReservation = await loadHtml("./projectPages/reservation/reservationData.html")
+    const templateBooking = await loadHtml("./projectPages/reservation/booking.html")
+    const templateActivities = await loadHtml("./projectPages/activities/activityOverview.html")
+    const templateNotFound = await loadHtml("./pages/notFound/notFound.html")
+    const templateFindReservation = await loadHtml("./ProjectPages/reservation/findReservation.html")
+  
+    adjustForMissingHash()
+  
+    const router = new Navigo("/", { hash: true });
+    //Not especially nice, BUT MEANT to simplify things. Make the router global so it can be accessed from all js-files
+    window.router = router
+  
+    router
+      .hooks({
+        before(done, match) {
+          setActiveLink("menu", match.url)
+          done()
+        }
+      })
+      .on({
+    
+        "/": () => renderTemplate(templatehome, "content"),
+  
+        "/addBike": () => renderTemplate(templateAbout, "content"),
+  
+        "/showBikes": () => {
+          renderTemplate(templateReservation, "content")
+        },
+     
+      })
+      .notFound(() => {
+        renderTemplate(templateNotFound, "content")
+      })
+      .resolve()
+  });
+  
+  
+  window.onerror = function (errorMsg, url, lineNumber, column, errorObj) {
+    alert('Error: ' + errorMsg + ' Script: ' + url + ' Line: ' + lineNumber
+      + ' Column: ' + column + ' StackTrace: ' + errorObj);
+  }
